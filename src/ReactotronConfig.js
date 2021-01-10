8ea6af010bf1ea39fb4cde342749aae9214129ba
dxwebster <adrianalimafm@gmail.com>
2021-01-10 20:14:56 -0300

    Add reactotron

--- src/ReactotronConfig.js
+++ src/ReactotronConfig.js
@@ -1,5 +1,10 @@
-import Reactotron from 'reactotron-react-js'
+// ReactotronConfig.js
+import { reactotronRedux } from 'reactotron-redux';
+import Reactotron from 'reactotron-react-js';
 
-Reactotron
-  .configure() // we can use plugins here -- more on this later
-  .connect() // let's connect!
+// then add it to the plugin list
+const reactotron = Reactotron.configure({ name: 'React JS' })
+  .use(reactotronRedux()) //  <- here i am!
+  .connect(); //Don't forget about me!
+
+export default reactotron;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux';
+import Reactotron from '../ReactotronConfig';
 
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, Reactotron.createEnhancer());
 
 export default store;
