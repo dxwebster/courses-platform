e34c527ac6207630f16474431b2f4f4d100332c9
dxwebster <adrianalimafm@gmail.com>
2021-01-12 21:04:45 -0300

    fix background

--- src/pages/Dashboard/components/Video/styles.js
+++ src/pages/Dashboard/components/Video/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   flex: 1;
+  height: calc(100vh - 50px);
   display: flex;
   flex-direction: column;
   gap: 30px;
@@ -13,6 +14,6 @@ export const Container = styled.div`
   iframe {
     margin: 20px 40px 0;
     width: 90%;
-    height: 400px;
+    height: 50%;
   }
 `;
