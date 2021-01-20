3a0fd833c78d9d51b4f9706a23671d664cd8d9d2
dxwebster <adrianalimafm@gmail.com>
2021-01-19 21:24:28 -0300

    dasnsa

---
+++ src/pages/Course/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const Card = styled.div`
+  max-width: 100%;
+  width: 100%;
+  color: #fff;
+
+  display: flex;
+`;
